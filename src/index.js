bdf8b75a26300af48d323c1dc00cacb494cc4b40
dante <c.dante.federici@gmail.com>
2019-04-04 01:55:18 -0400

    Downloading and deleting.

--- src/index.js
+++ src/index.js
@@ -14,6 +14,13 @@ const defaultState = {
 };
 const RECORD_EVENT = 'LifeTracker:RECORD_EVENT';
 const recordEvent = name => createAction(RECORD_EVENT, { name, at: Date.now() });
+
+const DOWNLOAD_DATA = 'LifeTracker:DOWNLOAD_DATA';
+const downloadData = () => createAction(DOWNLOAD_DATA);
+
+const DELETE_DATA = 'LifeTracker:DELETE_DATA';
+const deleteData = () => createAction(DELETE_DATA);
+
 const reducer = (state = defaultState, action) => {
 	switch (action.type) {
 		case RECORD_EVENT:
@@ -21,11 +28,20 @@ const reducer = (state = defaultState, action) => {
 				...state,
 				events: state.events.concat(action.payload),
 			};
+
+		case DELETE_DATA: {
+			return {
+				...state,
+				events: [],
+			};
+		}
+
+
 		default:
+			console.log('Unhandled', action);
 			return state;
 	}
 };
-
 
 const PERSIST_KEY = 'life-tracker-storage';
 const initialState = (() => {
@@ -37,15 +53,57 @@ const initialState = (() => {
 })();
 let shouldSave = false;
 const store = createStore(reducer, initialState, applyMiddleware(
+	// @todo: persistence
 	() => next => action => {
 		shouldSave = true;
 		return next(action);
+	},
+	// @todo: redux-logger
+	({ getState }) => next => action => {
+		const before = getState();
+		let res;
+		let isError = false;
+		try {
+			res = next(action);
+		} catch (error) {
+			res = error;
+			isError = true;
+		}
+		const after = getState();
+		console.groupCollapsed(`[${isError ? 'ERROR' : 'EVENT'} ${new Date().toISOString()}] ${action.type}`);
+		console.log(before);
+		console.log(action);
+		console.log(after);
+		console.groupEnd();
+		return res;
+	},
+	// DOWNLOAD
+	({ getState }) => next => action => {
+		if (action.type !== DOWNLOAD_DATA) {
+			return next(action);
+		}
+
+		console.log(getState());
+		try {
+			const a = document.createElement('a');
+			const file = new Blob([JSON.stringify(getState().events)], { type: 'application/json' });
+			const url = URL.createObjectURL(file);
+			a.href = url;
+			a.download = 'life-tracker.json';
+			document.body.appendChild(a);
+			a.click();
+			setTimeout(() => {
+				document.body.removeChild(a);
+				URL.revokeObjectURL(url);
+			});
+		} catch (error) {
+			console.log('!!!', error);
+		}
 	})
 );
 setInterval(() => {
 	if (shouldSave) {
 		window.localStorage.setItem(PERSIST_KEY, JSON.stringify(store.getState()));
-		console.info('Saved state.');
 	}
 }, 15 * 1000);
 
@@ -131,11 +189,18 @@ const LinkedSimpleEvent = connect(
 
 // --------------------- ADMIN TOOLS --------------- //
 const AdminTools = ({
-	eventsBarData,
-	eventsByHour,
+	eventsBarData, // { label: String, count: Number }[]
+	eventsByHour, // { hour: 0-23, data: { label: String, count: Number }[] }[]
+	downloadData, // () => {}, callback to fire
+	deleteData, // () => {}, callback to fire
 }) => h('section.admin-tools', [
 	h('h3', ['Admin Tools']),
 	h('section.data-sets', [
+		h('button', {
+			onClick: downloadData,
+		}, [
+			'Download Data (json blob)'
+		]),
 		h('h4', 'Events'),
 		h('ul', [
 			...eventsBarData.map(pt => h('li', [
@@ -152,7 +217,15 @@ const AdminTools = ({
 					])),
 				])
 			])),
-		])
+		]),
+		h('.danger-zone', [
+			h('h4', 'Danger Zone'),
+			h('button.danger', {
+				onClick: deleteData,
+			}, [
+				'Delete Data',
+			]),
+		]),
 	]),
 ]);
 
@@ -181,6 +254,10 @@ const LinkedAdminTools = connect(
 			fp.map(pair => ({ hour: pair[0], data: pair[1] })),
 		)(state.events),
 	}),
+	{
+		downloadData,
+		deleteData,
+	}
 )(AdminTools);
 
 
@@ -200,11 +277,3 @@ const App = () => h('section.life-tracker', [
 render(h(Provider, { store }, [
 	h(App),
 ]), document.body);
-
-setInterval(() => {
-	store.dispatch(recordEvent(fp.sample([
-		'Brushed Teeth',
-		'10 second plank',
-		'stretch',
-	])));
-}, 150);
